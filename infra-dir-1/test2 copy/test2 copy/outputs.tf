35cc9d0d945f539810637d12c00a87a948591540
Bojan Cakar <bojan.cakar@cyberlab.rs>
2023-08-21 09:38:32 +0200

    test

--- infra-dir-1/test2 copy/test2 copy/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_publicip" {
-  description = "EC2 instance public ip"
-  value       = aws_instance.myec2vm.public_ip
-}
-
-output "instance_publicdns" {
-  description = "EC2 instance public dns"
-  value       = aws_instance.myec2vm.public_dns
-}
