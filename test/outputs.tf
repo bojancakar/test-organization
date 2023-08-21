09dcf328048131ad5d31160a13314425904f0863
bojancakar <118173598+bojancakar@users.noreply.github.com>
2023-08-21 10:11:48 +0200

    Update outputs.tf

--- test/outputs.tf
+++ test/outputs.tf
@@ -1,9 +1,23 @@
-output "instance_publicip" {
-  description = "EC2 instance public ip"
-  value       = aws_instance.myec2vm.public_ip
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
 }
 
-output "instance_publicdns" {
-  description = "EC2 instance public dns"
-  value       = aws_instance.myec2vm.public_dns
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
